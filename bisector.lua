5f7c25e300ae158c9a89b7087c2f300fbb259cc5
Günther Eder <gue@cosy.sbg.ac.at>
2015-07-22 14:52:01 +0200

    GPL added

--- bisector.lua
+++ bisector.lua
@@ -7,6 +7,19 @@
 -- File used as a kind of template to create this: 
 -- (http://www.filewatcher.com/p/ipe_7.1.1-1_i386.deb.1106034/usr/lib/ipe/7.1.1/ipelets/euclid.lua.html)
 --
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
 
 label = "Bisector"
 
